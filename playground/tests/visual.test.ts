a1f4c8b4fd12d20286e2596f7bd438a6b27837f5
SegaraRai <SegaraRai@users.noreply.github.com>
2024-06-26 02:32:26 +0900

    test: add VRT

---
+++ playground/tests/visual.test.ts
@@ -0,0 +1,47 @@
+import {
+  chromium,
+  type Browser,
+  type BrowserContext,
+  type Page,
+} from "@playwright/test";
+import { configureToMatchImageSnapshot } from "jest-image-snapshot";
+import { fileURLToPath } from "node:url";
+import { afterAll, beforeAll, it, expect } from "vitest";
+
+const ORIGIN = "http://localhost:5188";
+
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+  customSnapshotsDir: fileURLToPath(
+    new URL("./__image_snapshots__", import.meta.url)
+  ),
+  failureThresholdType: "percent",
+  failureThreshold: 0.01,
+});
+expect.extend({ toMatchImageSnapshot });
+
+let browser: Browser;
+let context: BrowserContext;
+let page: Page;
+beforeAll(async () => {
+  browser = await chromium.launch();
+  context = await browser.newContext();
+  page = await context.newPage();
+});
+
+afterAll(async () => {
+  await browser.close();
+});
+
+it("index.html", async () => {
+  await page.goto(`${ORIGIN}/`);
+
+  const image = await page.screenshot();
+  expect(image).toMatchImageSnapshot();
+});
+
+it("secondary.html", async () => {
+  await page.goto(`${ORIGIN}/secondary`);
+
+  const image = await page.screenshot();
+  expect(image).toMatchImageSnapshot();
+});
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,7 +4,8 @@ import modularTailwindCSSPluginPrebuilt from "vite-plugin-modular-tailwindcss";
 import modularTailwindCSSPluginDev from "../src/index";
 
 const usePrebuilt =
-  !!env.CI || env.NODE_ENV === "test" || env.MTW_PLUGIN === "prebuilt";
+  env.MTW_PLUGIN !== "development" &&
+  (!!env.CI || env.NODE_ENV === "test" || env.MTW_PLUGIN === "prebuilt");
 
 const modularTailwindCSSPlugin = usePrebuilt
   ? modularTailwindCSSPluginPrebuilt
