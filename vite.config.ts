cbd6e658bf3b97283f9b71b0fc0e30b5399a79a4
SegaraRai <SegaraRai@users.noreply.github.com>
2024-06-27 23:55:36 +0900

    fix(build): exclude tests from dts processing

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
   plugins: [
     externalizeDeps(),
     dts({
+      exclude: ["tests/**"],
       tsconfigPath: "tsconfig.lib.json",
       compilerOptions: { baseUrl: "src" },
       rollupTypes: true,
