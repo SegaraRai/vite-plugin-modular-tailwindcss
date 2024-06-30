56171f11fdcb66e2eb47944475a92616e792ab33
SegaraRai <SegaraRai@users.noreply.github.com>
2024-07-01 00:43:24 +0900

    ci: handle cases where changeset releases are empty

--- scripts/nextVersion.ts
+++ scripts/nextVersion.ts
@@ -1,18 +1,22 @@
 import { spawnSync } from "node:child_process";
 import { readFile } from "node:fs/promises";
 
+interface ChangesetStatus {
+  changesets: unknown[];
+  releases: { name: string; newVersion: string }[];
+}
+
 spawnSync("pnpm", ["changeset", "status", "--output", "changeset-status.json"]);
 
 const changesetStatus = JSON.parse(
   await readFile("changeset-status.json", "utf8")
-) as { releases: { name: string; newVersion: string }[] };
+) as ChangesetStatus;
 const release = changesetStatus.releases.find(
   (release) => release.name === "vite-plugin-modular-tailwindcss"
 );
 
-if (!release) {
-  console.error("Release not found in changeset status");
-  process.exit(1);
+if (release) {
+  console.log(release.newVersion);
+} else {
+  console.error("Not ready for release yet.");
 }
-
-console.log(release.newVersion);
